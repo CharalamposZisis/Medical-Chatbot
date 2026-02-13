fb51f0b2d001d341abccbff4b55eae95e590e474
Charalampos <charalamposzeses282@gmail.com>
2026-02-13 16:08:51 +0200

    Second commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name = "medical-chatbot",
+    version="0.1.0",
+    author='Mpampis Zisis',
+    author_email='xarisban@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
